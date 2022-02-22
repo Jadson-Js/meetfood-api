fd03d83402ec992c73688b0931c8bcd57173f883
Jadson-Js <jadson20051965@gmail.com>
2022-02-21 21:56:28 -0300

    adding boot to server

--- config/index.js
+++ config/index.js
@@ -3,5 +3,12 @@ const dotenv = require('dotenv');
 module.exports = {
   app: {
     port: process.env.APP_PORT || 3000
+  },
+  database: {
+    host: process.env.DATABSE_HOST || '127.0.0.1',
+    port: process.env.DATABSE_PORT || 3306,
+    user: process.env.DATABSE_USER || 'root',
+    password: process.env.DATABSE_PASSWORD || 'Powerranger123!',
+    database: process.env.DATABSE_DATABASE || 'meetfood',
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,6 @@
 require('dotenv').config()
 require('module-alias/register')
 
-const app = require('@app')
-const config = require('config')
+const boot = require('@api/services/boot')
 
-app.listen(config.app.port, (error) => {
-    if (error) {
-        return console.log('Unespected error')
-    } else {
-        console.log(`Start in http://localhost:${config.app.port}`)
-    }
-})
+boot()
