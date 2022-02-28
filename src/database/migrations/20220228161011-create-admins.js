cf350c71b12c7bf5215e3bf9a97db71820c48d2d
Jadson-Js <jadson20051965@gmail.com>
2022-02-28 13:14:08 -0300

    Added column name in users table and created a new table: admins

--- src/database/migrations/20220228161011-create-admins.js
+++ src/database/migrations/20220228161011-create-admins.js
@@ -2,13 +2,20 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('admins', {
       id: {
         type: Sequelize.INTEGER,
         unique: true,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: false,
+        autoIncrement: false,
+        allowNull: false,
+        primaryKey: false
       },
       email: {
         type: Sequelize.STRING,
